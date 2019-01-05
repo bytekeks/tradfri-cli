a02eac9da76221672fc08f5c88ac24b8723a52d4
bytekeks <hello@bytekeks.de>
2019-01-05 20:02:24 +0100

    add command npm package

--- index.js
+++ index.js
@@ -2,22 +2,45 @@
 
 const debug = require('debug')('tradfri-cli:index');
 const api = require('./api');
+const program = require('commander');
 
-const doit = async () => {
+const app = async () => {
+  program
+    .version('0.0.1', '-v, --version')
+    .usage('[options]')
+    .option('-b, --brightness <percentage>', 'Set brightness of all lights', parseInt)
+    .option('-t, --temp <color_temperature>', 'Set color temperature for all lights', /^(white|glow|warm)$/i, 'warm')
+    .option('-off, --switchoff', 'Switch off all lights')
+//    .option('-on, --switchon', 'Switch on all lights')
+    .parse(process.argv);
+
   const myLamps = new api();
 
   await myLamps.connect();
 
-  //await myLamps.switchOff('MyCoolLampName');
+  if (program.brightness) {
+    // console.log('%j', program.brightness);
+    await myLamps.setBrightness('Schreibtisch', program.brightness);
+    await myLamps.setBrightness('Schrank', program.brightness);
+  }
 
-  // await myLamps.switchOn('MyCoolLampName');
+  if (program.temp && !program.switchoff) {
+    // console.log('%j', program.temp);
+    await myLamps.setColour('Schreibtisch', program.temp);
+    await myLamps.setColour('Schrank', program.temp);
+  }
 
-  await myLamps.setBrightness('MyCoolLampName', 100);
+  if (program.switchoff) {
+    await myLamps.switchOff('Schrank');
+    await myLamps.switchOff('Schreibtisch');
+  }
 
-  // glow, white, warm
-  await myLamps.setColour('MyCoolLampName', 'white');
+  // if (program.switchoon) {
+  //   await myLamps.switchOn('Schrank');
+  //   await myLamps.switchOn('Schreibtisch');
+  // }
 
   await myLamps.close();
 };
 
-doit();
+app();
